c3c3adee8e4deaff144fdde90b06eddce45fab25
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-20 14:14:17 -0500

    Update Order.cs

--- Parcial/src/Domain/Entities/Order.cs
+++ Parcial/src/Domain/Entities/Order.cs
@@ -7,10 +7,5 @@ namespace Domain.Entities
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
-
-
-
-        // O si lo necesitas para alguna interfaz, simplemente déjalo vacío (solo si realmente hace falta):
-        // public void CalculateTotal() { /* sin lógica, advertencia desactivada */ }
     }
 }
