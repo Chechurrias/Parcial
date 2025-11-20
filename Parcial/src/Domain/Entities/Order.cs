f58077d0f86c7936e7a02fbf2338fc9ab1a135d5
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-19 19:22:30 -0500

    Update Order.cs

--- Parcial/src/Domain/Entities/Order.cs
+++ Parcial/src/Domain/Entities/Order.cs
@@ -1,45 +1,17 @@
-public class Order
+namespace Domain.Entities
 {
-    private int _id;
-    private string _customerName;
-    private string _productName;
-    private int _quantity;
-    private decimal _unitPrice;
-
-    // Propiedad pública con control completo
-    public int Id
+    public class Order
     {
-        get { return _id; }
-        private set { _id = value; } // set privado si solo la clase la modifica
-    }
+        public int Id { get; private set; }
+        public string CustomerName { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
 
-    public string CustomerName
-    {
-        get { return _customerName; }
-        set { _customerName = value; }
-    }
-
-    public string ProductName
-    {
-        get { return _productName; }
-        set { _productName = value; }
-    }
-
-    public int Quantity
-    {
-        get { return _quantity; }
-        set { _quantity = value; }
-    }
-
-    public decimal UnitPrice
-    {
-        get { return _unitPrice; }
-        set { _unitPrice = value; }
-    }
-
-    public void CalculateTotalAndLog()
-    {
-        var total = Quantity * UnitPrice;
-        Infrastructure.Logging.Logger.Log($"Total: {total}");
+        public void CalculateTotalAndLog()
+        {
+            var total = Quantity * UnitPrice;
+            Infrastructure.Logging.Logger.Log($"Total: {total}");
+        }
     }
 }
