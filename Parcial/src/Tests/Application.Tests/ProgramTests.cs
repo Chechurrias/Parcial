7e0730f4cfd7b3f6ad62f3caebbf11c7488ec47f
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-20 12:39:09 -0500

    Update ProgramTests.cs

--- Parcial/src/Tests/Application.Tests/ProgramTests.cs
+++ Parcial/src/Tests/Application.Tests/ProgramTests.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using WebApi;
 using Application.Interfaces;
+using Application.Test;
 
 public class CustomWebAppFactory : WebApplicationFactory<Program>
 {
