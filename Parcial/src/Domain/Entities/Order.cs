a8ba5b86b535e32b9a29a1225adfd3d45cffc925
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-19 23:17:33 -0500

    Update Order.cs

--- Parcial/src/Domain/Entities/Order.cs
+++ Parcial/src/Domain/Entities/Order.cs
@@ -2,7 +2,7 @@ namespace Domain.Entities
 {
     public class Order
     {
-        public int Id { get; private set; }
+        public int Id { get; } // Setter privado eliminado
         public string CustomerName { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
@@ -11,8 +11,7 @@ namespace Domain.Entities
         public void CalculateTotal()
         {
             var total = Quantity * UnitPrice;
-            // Eliminado: Infrastructure.Logging.Logger.Log(...)
-            // Si necesitas avisar/loggear, hazlo desde capas superiores (Application, Infrastructure, WebApi)
+            // Logica de logging eliminada, delegada a Application/Infra
         }
     }
 }
