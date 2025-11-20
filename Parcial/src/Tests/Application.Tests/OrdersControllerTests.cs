0b9239ad3888b2419b6601ac674a05578b086f49
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-20 11:43:36 -0500

    Create OrdersControllerTests.cs

---
+++ Parcial/src/Tests/Application.Tests/OrdersControllerTests.cs
@@ -0,0 +1,16 @@
+using WebApi.Controllers;
+using Xunit;
+
+public class OrdersControllerTests
+{
+    [Fact]
+    public void DoNothing_ReturnsExpectedMessage()
+    {
+        // Act
+        var mensaje = OrdersController.DoNothing();
+
+        // Assert
+        Assert.Equal(OrdersController.Message, mensaje);
+        Assert.Equal("This controller does nothing. Endpoints are in Program.cs", mensaje);
+    }
+}
