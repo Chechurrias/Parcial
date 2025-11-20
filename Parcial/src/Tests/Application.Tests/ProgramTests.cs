76cf41754ec26e4231b548de1e4fcd27dae684f4
Chechurrias <161087269+Chechurrias@users.noreply.github.com>
2025-11-20 10:45:33 -0500

    Update ProgramTests.cs

--- Parcial/src/Tests/Application.Tests/ProgramTests.cs
+++ Parcial/src/Tests/Application.Tests/ProgramTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
+using WebApi;
 
 public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
 {
